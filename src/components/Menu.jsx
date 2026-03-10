d3bbd987ca4e1a1616cef727447ae3f62e205a34
Ana Beatriz <ana.freitas0046@gmail.com>
2026-03-09 23:48:36 -0300

    mudanças no layout

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,31 +1,48 @@
-import { Link } from "react-router-dom"
-import { useState } from "react"
-import { FaBars } from "react-icons/fa"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
-function Menu(){
+function Menu() {
+  const [open, setOpen] = useState(false);
 
-  const [open,setOpen] = useState(false)
-
-  return(
+  return (
     <nav>
+      <div className="logo">
+        <Link to="/">TechSolutions</Link>
+      </div>
 
-      <div className="logo">TechSolutions</div>
-
-      <div className="hamburger" onClick={()=>setOpen(!open)}>
-        <FaBars/>
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        <FaBars />
       </div>
 
       <div className={`menu-links ${open ? "active" : ""}`}>
-
-        <Link className="menu-link" to="/">Home</Link>
-        <Link className="menu-link" to="/produtos">Produtos</Link>
-        <Link className="menu-link" to="/empresa">Empresa</Link>
-        <Link className="menu-link" to="/contacts">Contatos</Link>
-
+        <Link className="menu-link" to="/" onClick={() => setOpen(false)}>
+          Home
+        </Link>
+        <Link
+          className="menu-link"
+          to="/produtos"
+          onClick={() => setOpen(false)}
+        >
+          Produtos
+        </Link>
+        <Link
+          className="menu-link"
+          to="/empresa"
+          onClick={() => setOpen(false)}
+        >
+          Empresa
+        </Link>
+        <Link
+          className="menu-link"
+          to="/contacts"
+          onClick={() => setOpen(false)}
+        >
+          Contatos
+        </Link>
       </div>
-
     </nav>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
